dc7db96b2cc544dfab36ba633c845e12d8dcbdf9
Seisl Manuel <1810653153@stud.fh-kufstein.ac.at>
2018-11-08 09:24:12 +0100

    Work with escape sequences

--- src/package3/Main.java
+++ src/package3/Main.java
@@ -4,17 +4,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-
-        String name = "Manuel Seisl";
-        int age = 26;
-
-        // Variablenname von fachhochschuleKufsteinTirol auf fhKufsteinTirol gekürzt und in Konstante umgewandelt
-        final String FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol";
-
-        final String NEW_CONSTANT;
-
-        System.out.println(name);
-        System.out.println(age);
-
+        System.out.println("Durch \\ kann man auch den Backslash selbst beliebig oft ausgeben: \\\\\\\\");
+        System.out.println("Ich verwende den\b Backspace, die Newline\n und den \tTabulator.");
+        System.out.println("Durch den Wagenrücklauf wird die Zeile zurückgesetzt und der Teil nach dem \r überschreibt\n" + "die bestehende Zeile. \f ist in der Konsole nicht bemerkbar, da es einen Seitenumbruch\n" + "erzwingt.");
     }
 }
