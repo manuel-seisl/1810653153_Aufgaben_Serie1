22e0fdc751beeac11bc01e39d6b830d9d4ef59e1
Seisl Manuel <1810653153@stud.fh-kufstein.ac.at>
2018-11-08 09:34:00 +0100

    Adapted backslashes from 8 to 4

--- src/package3/Main.java
+++ src/package3/Main.java
@@ -20,7 +20,7 @@ public class Main
         System.out.println(age);
 
         // Ausgabe Escape Sequenzen
-        System.out.println("Durch \\ kann man auch den Backslash selbst beliebig oft ausgeben: \\\\\\\\");
+        System.out.println("Durch \\ kann man auch den Backslash selbst beliebig oft ausgeben: \\\\");
         System.out.println("Ich verwende den\b Backspace, die Newline\n und den \tTabulator.");
         System.out.println("Durch den Wagenrücklauf wird die Zeile zurückgesetzt und der Teil nach dem \r überschreibt\n" + "die bestehende Zeile. \f ist in der Konsole nicht bemerkbar, da es einen Seitenumbruch\n" + "erzwingt.");
 
