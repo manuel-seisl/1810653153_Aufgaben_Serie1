b4bcb911fc30af587ebb0a10681644b81302a04d
Seisl Manuel <1810653153@stud.fh-kufstein.ac.at>
2018-11-08 09:11:30 +0100

    Add new package and class for output name and age

---
+++ src/package3/Main.java
@@ -0,0 +1,15 @@
+package package3;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        String name = "Manuel Seisl";
+        int age = 26;
+
+        System.out.println(name);
+        System.out.println(age);
+
+    }
+}
