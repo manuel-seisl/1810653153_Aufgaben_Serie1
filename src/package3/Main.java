b39e04833c0f0a23be4a2ed8371c3530c40240be
Seisl Manuel <1810653153@stud.fh-kufstein.ac.at>
2018-11-08 09:16:14 +0100

    Rename variable and convert into constant

--- src/package3/Main.java
+++ src/package3/Main.java
@@ -8,6 +8,9 @@ public class Main
         String name = "Manuel Seisl";
         int age = 26;
 
+        // Variablenname von fachhochschuleKufsteinTirol auf fhKufsteinTirol gekürzt und in Konstante umgewandelt
+        final String FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol";
+
         System.out.println(name);
         System.out.println(age);
 
