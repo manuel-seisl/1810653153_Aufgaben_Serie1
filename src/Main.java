e5af2b8db687c885613f58246a4a463bc83d9e00
Seisl Manuel <1810653153@stud.fh-kufstein.ac.at>
2018-11-08 09:07:55 +0100

    Add comments

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
+        // Ausgabe Willkommensnachricht und Name
         System.out.println("Hello World!");
         System.out.println("Manuel Seisl");
     }
--- src/package2/FH.java
+++ src/package2/FH.java
@@ -4,6 +4,7 @@ public class FH {
 
     public static void main(String[] args)
     {
+        // Ausgabe FH
         System.out.println("FH Kufstein Tirol");
     }
 
